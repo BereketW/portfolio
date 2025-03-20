d3bdb3dbfcf959061d652cbd5f5f224d605c5c0a
BereketW <bereketsodenoo@gmail.com>
2025-03-20 15:33:07 +0300

    added: works list modal upon click implement both style and js functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 const services = document.querySelector("#services");
+const worksModal = document.querySelector(".works-modal");
+const modalCard = document.querySelector(".modal-card");
+const btnClose = document.querySelector(".btn-close");
+const workBtn = document.querySelector(".work-btn");
+console.log(modalCard);
+btnClose.addEventListener("click", () => {
+  worksModal.classList.add("display-none");
+  //   modalCard.style.display = "none";
+  console.log("Hello");
+});
+
+workBtn.addEventListener("click", () => {
+  worksModal.classList.remove("display-none");
+});
