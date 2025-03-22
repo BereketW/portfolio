b7d37031c6ef51b6decfec6505b18bf54dbe67db
BereketW <bereketsodenoo@gmail.com>
2025-03-22 15:36:38 +0300

    added: about hover container and cards

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const worksModal = document.querySelector(".works-modal");
 const modalCard = document.querySelector(".modal-card");
 const btnClose = document.querySelector(".btn-close");
 const workBtn = document.querySelector(".work-btn");
+const aboutBtn = document.querySelector(".about-btn");
+const abouts = document.querySelectorAll(".about");
+const aboutCardContainer = document.querySelector(".about-hover-card");
 console.log(modalCard);
 btnClose.addEventListener("click", () => {
   worksModal.classList.add("display-none");
@@ -12,4 +15,22 @@ btnClose.addEventListener("click", () => {
 
 workBtn.addEventListener("click", () => {
   worksModal.classList.remove("display-none");
+});
+
+document.addEventListener("keydown", () => {
+  worksModal.classList.add("display-none");
+});
+
+aboutBtn.addEventListener("mouseenter", () => {
+  // aboutCardContainer.style.display = " block";
+  // aboutCardContainer.style.transfom = "translateY(0%)";
+  aboutCardContainer.classList.add("show-about");
+
+  // console.log(aboutCardContainer);
+  // console.log("Hello Mouse");
+});
+aboutBtn.addEventListener("mouseleave", () => {
+  aboutCardContainer.classList.remove("show-about");
+  // console.log(aboutCardContainer);
+  // console.log("Hello Mouse");
 });
